aa7275ed75c576519a47df191dd78823630268a6
Evgeniia SU <janeu@mail.ru>
2025-10-02 15:56:22 +1000

    24 Обработка событий 2

--- local/app/Events/CrmHandler.php
+++ local/app/Events/CrmHandler.php
@@ -2,15 +2,15 @@
 
 namespace Events;
 
+use CIblockElement; 
 use Bitrix\Main\Loader;
 
 class CrmHandler
 {
     public static function OnAfterDealUpdate(&$arFields) 
     {
-        // получаем ID текущей сделки
-
- 		$dealObject = new \CCrmDeal();
+        //получаем ID текущей сделки
+  		$dealObject = new \CCrmDeal();
 
         $factory = \Bitrix\Crm\Service\Container::getInstance()->getFactory(\CCrmOwnerType::Deal); 
         $item = $factory->getItem($arFields['ID']);
@@ -25,15 +25,18 @@ class CrmHandler
             $propertyValue = $arFields['OPPORTUNITY_ACCOUNT'].'|'.$arFields['ACCOUNT_CURRENCY_ID'];  // присваиваем значение 
             
             // изменяем только одно поле
-            Loader::includeModule('iblock');
-           file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logIAU.txt', '1) id zakaz -: '.var_export($elementID, true).' сумма '.var_export($propertyValue, true).PHP_EOL, FILE_APPEND);
- 
-            //CIBlockElement::SetPropertyValues($elementID, 30,  $propertyValue, $propertyCode);
-            //CIBlockElement::SetPropertyValuesEx($elementID, 30,  array($propertyCode => $propertyValue));
-            CIBlockElement::SetPropertyValuesEx($elementID, 30,  [$propertyCode => $propertyValue]);
+            
+           if(Loader::IncludeModule('iblock')) {
+            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logIAU.txt', '1) id zakaz -: '.var_export($elementID, true).' сумма '.var_export($propertyValue, true).PHP_EOL, FILE_APPEND);
+    
+                //CIBlockElement::SetPropertyValues($elementID, 30,  $propertyValue, $propertyCode);
+                //CIBlockElement::SetPropertyValuesEx($elementID, 30,  array($propertyCode => $propertyValue));
+                //self::$handlerDisallow = true; 
+                CIBlockElement::SetPropertyValuesEx($elementID, 30,  [$propertyCode => $propertyValue]);
+                //self::$handlerDisallow = false;
 
-            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logIAU.txt', '2) id zakaz -: '.var_export($elementID, true).' сумма '.var_export($propertyValue, true).PHP_EOL, FILE_APPEND);
-        
+                file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logIAU.txt', '2) id zakaz -: '.var_export($elementID, true).' сумма '.var_export($propertyValue, true).PHP_EOL, FILE_APPEND);
+            }
         
         }
 
